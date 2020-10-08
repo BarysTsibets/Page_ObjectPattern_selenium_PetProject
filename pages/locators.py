98224bad2b89b47730777848b3c01e46b94b5e38
Barys_Tsibets <molekyla_08@mail.ru>
2020-10-08 15:11:11 -0700

    LoginPage(Base Page) class created

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    Login_Link = (By.CSS_SELECTOR, "#registration_link")
+
+
+class LoginPageLocators:
+    Login_Form = (By.CSS_SELECTOR, "#login_form")
+    Register_Form = (By.CSS_SELECTOR, '#register_form')
