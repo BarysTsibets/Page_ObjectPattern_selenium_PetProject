930605838dad7a32b12e9398163df765a663efde
Barys_Tsibets <molekyla_08@mail.ru>
2020-10-13 16:15:53 -0700

    asserts  in methods added

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,13 +11,13 @@ class ProductPage(BasePage):
         assert self.is_element_presented(*ProductPageLocators.Add_Basket_Button),\
             'No "Add to basket button" presented on this page'
 
-    def should_be_the_same_item_name_in_basket_and_confirmation(self):
+    def should_be_the_same_item_name_in_basket_and_in_confirmation(self):
         item_name = self.browser.find_element(*ProductPageLocators.Item_Name)
         conf_item = self.browser.find_element(*ProductPageLocators.Item_Name_Confirm_Message)
         assert item_name.text == conf_item.text, \
             'Another item name in confirmation message'
 
-    def should_match_item_price_and_item_price_in_basket(self):
+    def should_match_item_price_and_item_price_in_confirmation(self):
         item_price = self.browser.find_element(*ProductPageLocators.Item_Price)
         item_price_basket = self.browser.find_element(*ProductPageLocators.Item_Price_In_Message)
         # print(item_price.text)
