f7e4d0b625efc40b4b743de7c08adb5feb1bbbd5
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-14 22:17:59 -0700

    additional methods added

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,8 +8,16 @@ class ProductPage(BasePage):
         add_to_basket_button.click()
 
     def should_be_add_to_basket_button(self):
-        assert self.is_element_presented(*ProductPageLocators.Add_Basket_Button),\
+        assert self.is_element_presented(*ProductPageLocators.Add_Basket_Button), \
             'No "Add to basket button" presented on this page'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_presented(*ProductPageLocators.SUCCESS_MESSAGE), \
+            'Success message is presented , but should not be'
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared , but should  be"
 
     def should_be_the_same_item_name_in_basket_and_in_confirmation(self):
         item_name = self.browser.find_element(*ProductPageLocators.Item_Name)
@@ -22,8 +30,9 @@ class ProductPage(BasePage):
         item_price_basket = self.browser.find_element(*ProductPageLocators.Item_Price_In_Message)
         # print(item_price.text)
         # print(item_price_basket.text)
-        assert item_price.text == item_price_basket.text,\
+        assert item_price.text == item_price_basket.text, \
             "Item price and Item Price in confirmation message different"
+
 
 
 
