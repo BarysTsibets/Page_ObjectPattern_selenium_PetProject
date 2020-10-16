0e97551ab5aae909c62a67c2f9f730bd48585060
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-15 20:52:54 -0700

    test_guest_cant_see_product_in_basket_opened_from_product_page added

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -36,7 +37,7 @@ def test_guest_can_add_item_in_basket(browser, link):
     prod_page.should_match_item_price_and_item_price_in_confirmation()
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     prod_page = ProductPage(browser, prod_link)
@@ -45,14 +46,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     prod_page.should_not_be_success_message()
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     prod_page = ProductPage(browser, prod_link)
     prod_page.open()
     prod_page.should_not_be_success_message()
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     prod_page = ProductPage(browser, prod_link)
@@ -70,10 +71,16 @@ def test_guest_should_see_login_link_on_product_page(browser):
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    prod_page = ProductPage(browser,link)
+    prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.go_to_login_page()
 
 
-
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    prod_page = ProductPage(browser, prod_link)
+    prod_page.open()
+    prod_page.go_to_basket_page()
+    # BasketPage.should_not_be_items_in_the_basket()
+    prod_page.should_not_be_items_in_the_basket()
+    prod_page.should_be_your_basket_is_empty_message()
 
