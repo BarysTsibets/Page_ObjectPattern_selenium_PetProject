e0af40eaac72f78fe4099eb2a1d20035c8a898b2
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-15 20:52:22 -0700

    test_guest_cant_see_product_in_basket_opened_from_main_page added

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,39 @@
-from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
+import pytest
+import time
 
 
-promo_link = \
-    "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+# promo_link = \
+#     "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-
-
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_items_in_the_basket()
+    page.should_be_your_basket_is_empty_message()
+
+
+
+
+
