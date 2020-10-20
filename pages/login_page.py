27c226d0b39c247a3b2d9328e6fbd6df8bd0e41b
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-19 18:56:09 -0700

    register_new_user method added

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,30 @@
 from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
+import faker
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
 
 
+def fake_email_creator():
+    f = faker.Faker()
+    mail = f.mail()
+    return mail
+
+
+EMAIL_1 = fake_email_creator()
+PASW_1 = '123qwe!@#'
+
+
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.Email_Address_Field)
+        email_field.send_key(email)
+        password_field = self.browser.find_element(*LoginPageLocators.Password_Field)
+        password_field.send_key(password)
+        confirm_password = self.browser.find_element(*LoginPageLocators.Confirm_Password_Field)
+        confirm_password.send_key(password)
+        self.browser.find_element(*LoginPageLocators.Register_Button).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
