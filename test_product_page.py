961f64b28c538b63484126ee94f6267601c1ccc0
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-19 20:56:50 -0700

    TestUserAddToBasketFromProductPage class updated

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import time
 import pytest
 
-# The shellcoder's handbook" book
+# The Shellcoder's handbook" book
 prod_link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
 # "Coders at work" book
 prod_link2 = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
@@ -61,6 +62,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     prod_page.add_item_in_basket()
     prod_page.should_be_disappeared_success_message()
 
+
 @pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -68,12 +70,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     prod_page.open()
     prod_page.should_be_login_link()
 
+
 @pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.go_to_login_page()
+
 
 @pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
@@ -84,23 +88,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     prod_page.should_be_your_basket_is_empty_message()
 
 
+@pytest.mark.practice
 class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        login_link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        self.page = LoginPage(browser, login_link)
+        self.page.open()
+        Fake_email = str(time.time()) + '@fakemail.org'
+        Fake_password = '123qwe!@#'
+        self.page.register_new_user(Fake_email, Fake_password)
+        self.page.should_be_authorized_user()
+        time.sleep(5)
+
     def test_user_cant_see_success_message(self, browser):
         prod_page = ProductPage(browser, prod_link)
         prod_page.open()
         prod_page.should_not_be_success_message()
 
-    def test_user_can_add_item_in_basket(self, browser, link):
-        prod_page = ProductPage(browser, link)
+    def test_user_can_add_item_in_basket(self, browser):
+        prod_page = ProductPage(browser, prod_link)
         prod_page.open()
         prod_page.add_item_in_basket()
-        # Getting the code from alert window
-        prod_page.solve_quiz_and_get_code()
+        # prod_page.solve_quiz_and_get_code()
         prod_page.should_be_add_to_basket_button()
         # Item name and price confirmation
         prod_page.should_be_the_same_item_name_in_basket_and_in_confirmation()
         prod_page.should_match_item_price_and_item_price_in_confirmation()
-
-
-
-
