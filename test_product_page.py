33741655517ad38c080408278b1774087df45cae
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-19 19:00:14 -0700

    TestUserAddToBasketFromProductPage class created

--- test_product_page.py
+++ test_product_page.py
@@ -37,7 +37,7 @@ def test_guest_can_add_item_in_basket(browser, link):
     prod_page.should_match_item_price_and_item_price_in_confirmation()
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     prod_page = ProductPage(browser, prod_link)
@@ -46,14 +46,14 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     prod_page.should_not_be_success_message()
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     prod_page = ProductPage(browser, prod_link)
     prod_page.open()
     prod_page.should_not_be_success_message()
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     prod_page = ProductPage(browser, prod_link)
@@ -61,25 +61,46 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     prod_page.add_item_in_basket()
     prod_page.should_be_disappeared_success_message()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.should_be_login_link()
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.go_to_login_page()
 
-
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     prod_page = BasketPage(browser, prod_link)
     prod_page.open()
     prod_page.go_to_basket_page()
     prod_page.should_not_be_items_in_the_basket()
     prod_page.should_be_your_basket_is_empty_message()
+
+
+class TestUserAddToBasketFromProductPage:
+    def test_user_cant_see_success_message(self, browser):
+        prod_page = ProductPage(browser, prod_link)
+        prod_page.open()
+        prod_page.should_not_be_success_message()
+
+    def test_user_can_add_item_in_basket(self, browser, link):
+        prod_page = ProductPage(browser, link)
+        prod_page.open()
+        prod_page.add_item_in_basket()
+        # Getting the code from alert window
+        prod_page.solve_quiz_and_get_code()
+        prod_page.should_be_add_to_basket_button()
+        # Item name and price confirmation
+        prod_page.should_be_the_same_item_name_in_basket_and_in_confirmation()
+        prod_page.should_match_item_price_and_item_price_in_confirmation()
+
+
+
 
