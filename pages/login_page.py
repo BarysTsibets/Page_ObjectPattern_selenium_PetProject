20d55a4918e77be467e3987ffe1155e4cb4521e8
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-26 13:47:49 -0700

    minor refactoring

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-import faker
-
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
 
 
 class LoginPage(BasePage):
@@ -21,15 +18,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert 'login' in self.browser.current_url, "url don't have word 'login' ."
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_presented(*LoginPageLocators.Login_Form), \
             'Login form is not found on this page'
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_presented(*LoginPageLocators.Register_Form), \
             'Register form is not found on this page'
