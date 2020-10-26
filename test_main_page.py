d64c50b5a61fd406358292d74b0b1cf60fb3e249
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-10-26 13:59:36 -0700

    refactoring (functions --> class)

--- test_main_page.py
+++ test_main_page.py
@@ -2,10 +2,9 @@ from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 import pytest
-import time
 
-# promo_link = \
-#     "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+promo_link = \
+    "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
 link = "http://selenium1py.pythonanywhere.com/"
 
 
@@ -30,26 +29,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     page.should_not_be_items_in_the_basket()
     page.should_be_your_basket_is_empty_message()
-
-
-# Just Example: Adding and deleting Book through API
-# @pytest.mark.login
-# class TestLoginFromProductPage():
-#     @pytest.fixture(scope="function", autouse=True)
-#     def setup(self):
-#         self.product = ProductFactory(title="Best book created by robot")
-#         # создаем по апи
-#         self.link = self.product.link
-#         yield
-#         # после этого ключевого слова начинается teardown
-#         # выполнится после каждого теста в классе
-#         # удаляем те данные, которые мы создали
-#         self.product.delete()
-#
-#     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-#         page = ProductPage(browser, self.link)
-#         # дальше обычная реализация теста
-#
-#     def test_guest_should_see_login_link(self, browser):
-#         page = ProductPage(browser, self.link)
-#         # дальше обычная реализация теста
